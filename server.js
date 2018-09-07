5fb046a3dd0626445387b8db01e4aa7b107ece5e
Kamil Gumienny <kamilgummy@outlook.com>
2018-09-07 13:41:33 -0400

    Added aditional parameter options and modified API call

--- server.js
+++ server.js
@@ -26,156 +26,136 @@ var data = [
 var server = http.createServer(function(req, res) {
   var uri = url.parse(req.url)
   if (req.method === 'GET') {
-      switch (uri.pathname) {
-        case '/':
-          sendFile(res, 'public/index.html')
-          break;
-        case '/index.html':
-          sendFile(res, 'public/index.html')
-          console.log('ldwadawdawd0')
-          break;
-        case '/css/style.css':
-          sendFile(res, 'public/css/style.css', 'text/css')
-          break;
-        case '/js/scripts.js':
-          sendFile(res, 'public/js/scripts.js', 'text/javascript')
-          break;
-        case '/yes':
-          console.log('yeeet')
-          break;
-        case '/barImage.jpg':
-          sendFile(res, 'barImage.jpg')
-          break;
-        case '/beerTapTrans.png':
-          sendFile(res, 'beerTapTrans.png')
-          break;
-        default:
-          res.end('404 not found')
-      }
-    }else if(req.method === 'POST'){
-      console.log("POST request");
-      var body = '';
-        req.on('data', function (data) {
-            body += data;
-        });
-        req.on('end', function () {
-            console.log("Body: " + body);
-            var formData = qs.parse(body);
-            console.log(formData.brewer);
-            doAPICall(req, res, formData)
-        });
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        //res.end('post received');
+    switch (uri.pathname) {
+      case '/':
+        sendFile(res, 'public/index.html')
+        break;
+      case '/index.html':
+        sendFile(res, 'public/index.html')
+        console.log('ldwadawdawd0')
+        break;
+      case '/css/style.css':
+        sendFile(res, 'public/css/style.css', 'text/css')
+        break;
+      case '/js/scripts.js':
+        sendFile(res, 'public/js/scripts.js', 'text/javascript')
+        break;
+      case '/yes':
+        console.log('yeeet')
+        break;
+      case '/barImage.jpg':
+        sendFile(res, 'barImage.jpg')
+        break;
+      case '/beerTapTrans.png':
+        sendFile(res, 'beerTapTrans.png')
+        break;
+      default:
+        res.end('404 not found')
+    }
+  } else if (req.method === 'POST') {
+    console.log("POST request");
+    var body = '';
+    req.on('data', function(data) {
+      body += data;
+    });
+    req.on('end', function() {
+      //console.log("Body: " + body);
+      var formData = qs.parse(body);
+      console.log(formData.brewer);
+      console.log(formData.categories);
+      console.log(formData.style);
+      console.log(formData.country);
 
-    }else{
-        get404(req, res);
-      }
+      doAPICall(req, res, formData)
+    });
+
+  } else {
+    get404(req, res);
   }
-)
+})
 
 server.listen(process.env.PORT || port);
 console.log('listening on 8080')
 
-function doAPICall(res, req, data){
+function doAPICall(res, req, data) {
   //separate the data
   //get the data needed for api call
   var alcLow = data.Alcohol1;
   var alcHigh = data.Alcohol2;
-  var style = data.style;
-  var category = data.categories;
-  var brewer = data.brewer;
-  var country = data.country;
+  var style ='';
+  var styleTrue = '';
+  var category='';
+  var categoryTrue = '';
+  var brewer='';
+  var brewerTrue = '';
+  var country='';
+  var countryTrue = '';
+
+  if(data.style !== 'Any'){
+    style = data.style;
+    styleTrue = '&refine.style_name=';
+  }
+
+  if(data.categories !== 'Any'){
+    category = data.category;
+    categoryTrue = '&refine.cat_name';
+  }
+
+  if(data.brewer !== 'Any'){
+    brewer = data.brewer;
+    brewerTrue = '&refine.name_breweries=';
+  }
+
+  if(data.country !== 'Any'){
+    style = data.country;
+    countryTrue = '&refine.country=';
+  }
   //organize the data
-  var apiUrl = 'http://data.opendatasoft.com/api/records/1.0/search/?dataset=open-beer-database%40public-us&sort=-abv&facet=style_name&facet=cat_name&facet=name_breweries&facet=country&refine.cat_name='+ category +'&refine.country=' + country +'&refine.name_breweries='+ brewer + '&refine.style_name='+ style;
+  var apiUrl = 'http://data.opendatasoft.com/api/records/1.0/search/?dataset=open-beer-database%40public-us&rows=6000&facet=style_name&facet=cat_name&facet=name_breweries&facet=country' + categoryTrue + category + countryTrue + country + brewerTrue + brewer + styleTrue + style;
+  console.log(apiUrl);
   //do the api call
-  var parsedData;
-  var cont = 0;
-
-  parsedData= makeCall(apiUrl, res, req, alcLow, alcHigh, processResponse);
-
-
-
-
-    //console.log(parsedData.records[1].fields.descript);
-
-    //Have to add a callback to wait for function to finish
-
-      /*
-      var go = 1;
-      var count;
-      for (count = 0; go != 0; x++){
-        if(parsedData.records[x].fields.descript != NULL){
-          continue;
-        }else{
-          go = 0;
-        }
-      }
-      console.log(count);
-
-      */
-      //var listSize = Object.keys(parsedData.records).length;
-
-    /*
-
-    //organize that data (find one between the alcohol value)
-    var alcByVolumeList;
-    for(var x = 0; x < parsedData.nhits; x++){
-      if (parsedData.records[x].fields.abv > alcLow && parsedData.records[x].fields.abv < alcHigh){
-        alcByVolumeList.push(beer);
-      }
-    }
-
-    */
-
-    /*
-    parsedData.records.forEach(function(beer)){
-      if (beer.fields.abv > alcLow && beer.fields.abv < alcHigh){
-        alcByVolumeList.push(beer);
-      }
-    }
-    */
-    //listSize = alcByVolumeList.length;
-    //console.log("There are " + listSize + " options.");
-  //send the data back with index.html
+  parsedData = makeCall(apiUrl, res, req, alcLow, alcHigh, processResponse);
 }
 
-  function makeCall(apiUrl, res, req, alcLow, alcHigh, callback){
-    http.get(apiUrl, (resp) => {
-      let data = '';
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
+function makeCall(apiUrl, res, req, alcLow, alcHigh, callback) {
+  var parsedData;
+  http.get(apiUrl, (resp) => {
+    let data = '';
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
 
-      // The whole response has been received. Print out the result.
-      resp.on('end', () => {
-        console.log(data);
-        parsedData = JSON.parse(data);
-        callback(res, req, alcLow, alcHigh, parsedData);
-        //console.log(parsedData.records[0].fields.descript);
-        //console.log(parsedData.records[1].fields.descript);
-        //console.log(parsedData.records.length);
-      });
-      }).on("error", (err) => {
-        console.log("Error: " + err.message);
-      });
-  }
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      //console.log(data);
+      parsedData = JSON.parse(data);
+      callback(res, req, alcLow, alcHigh, parsedData);
+    });
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+}
 
-  function processResponse(res, req, alcLow, alcHigh, parsedData){
-    console.log(parsedData.records[1].fields.descript);
-    console.log(parsedData.nhits);
-    var alcByVolumeList = [];
-    for(var x = 0; x < parsedData.nhits; x++){
-      if (parsedData.records[x].fields.abv > alcLow && parsedData.records[x].fields.abv < alcHigh){
-        alcByVolumeList.push(parsedData.records[x]);
-      }
+function processResponse(res, req, alcLow, alcHigh, parsedData) {
+  //console.log(parsedData.records[1].fields.descript);
+  console.log(parsedData.nhits);
+  var alcByVolumeList = [];
+  for (var x = 0; x < parsedData.nhits; x++) {
+    console.log(x);
+    if (parsedData.records[x].fields.abv >= alcLow && parsedData.records[x].fields.abv <= alcHigh) {
+      alcByVolumeList.push(parsedData.records[x]);
     }
-
-     console.log(alcByVolumeList);
   }
 
+  console.log(alcByVolumeList.length);
+
+  //res.writeHead(200, {'Content-Type': 'text/html'});
+  //res.end('post received');
 
 
-function get404(req, res){
+}
+
+function get404(req, res) {
   //fill in
 }
 // subroutines
@@ -187,7 +167,6 @@ function submitbyid(){
   console.log(brewer);
 }
 */
-
 
 function sendFile(res, filename, contentType) {
   contentType = contentType || 'text/html';
